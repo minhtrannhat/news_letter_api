38f964652667470a47fdf68f49cef7cc6c6f3cdd
minhtrannhat <minh@minhtrannhat.com>
2024-05-03 16:14:36 -0400

    feat: use configuration.yaml to setup variables

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::net::TcpListener;
 
-use email_newsletter_api::startup;
+use email_newsletter_api::{configuration::get_configuration, startup};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000");
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let port_number = configuration.application_port;
+
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port_number))
+        .unwrap_or_else(|_| panic!("Can't bind to port {} at localhost", port_number));
 
     // Move the error up the call stack
     // otherwise await for the HttpServer
